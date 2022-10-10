a68d77b03eddb8d58ba40ba72a6abf5324f9753c
Layton4 <Javierete4774@gmail.com>
2022-10-10 23:11:24 +0200

    10/10/22

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,23 +12,50 @@ public class PlayerController : MonoBehaviour
 
     public float xInput, yInput;
 
+    #region ClaseMartes
+    private bool isWalking = false;
+    private Vector2 lastMovement = Vector2.zero;
+    private Animator playerAnimator;
+    public const string LASTH = "LastHorizontal", LASTV = "LastVertical";
+    #endregion
+
+    private void Awake()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
     void Update()
     {
+        isWalking = false;
         xInput = Input.GetAxisRaw(HORIZONTAL);
         yInput = Input.GetAxisRaw(VERTICAL);
 
-        if(Mathf.Abs(xInput) > inputTol || Mathf.Abs(yInput) > inputTol)
+        if(Mathf.Abs(xInput) > inputTol)
         {
             Vector3 translation = new Vector3(xInput * speed * Time.deltaTime, 0, 0);
             //translation.y = yInput * speed * Time.deltaTime;
             transform.Translate(translation);
+
+            isWalking = true;
         }
 
         if (Mathf.Abs(yInput) > inputTol)
         {
             Vector3 translation_y = new Vector3(0, yInput * speed * Time.deltaTime, 0);
             transform.Translate(translation_y);
+
+            isWalking = true;
         }
         
     }
+    
+    private void LateUpdate()
+    {
+        playerAnimator.SetFloat(HORIZONTAL, xInput);
+        playerAnimator.SetFloat(VERTICAL, yInput);
+
+        playerAnimator.SetBool("IsWalking", isWalking);
+        playerAnimator.SetFloat(LASTH, lastMovement.x);
+        playerAnimator.SetFloat(LASTV, lastMovement.y);
+    }
+    
 }
