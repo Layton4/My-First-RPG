591e5114a37743c4470dd7f7eee70468565d0df6
Layton4 <73424805+Layton4@users.noreply.github.com>
2022-10-24 13:40:18 +0200

    Confiner arreglado

--- Assets/Scripts/StartPoint.cs
+++ Assets/Scripts/StartPoint.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class StartPoint : MonoBehaviour
 {
@@ -8,9 +9,12 @@ public class StartPoint : MonoBehaviour
     [SerializeField] private Vector2 facingDirection;
 
     public string uuid; //uuid means Universal Unique IDENTIFIER
+
+    private CinemachineConfiner2D virtualCameraObject;
     void Start()
     {
         playerControllerScript = FindObjectOfType<PlayerController>();
+        virtualCameraObject = FindObjectOfType<CinemachineConfiner2D>();
 
         if(!playerControllerScript.nextUuid.Equals(uuid)) //if my uuid don't match the player uuid stop reading the start
         {
@@ -19,6 +23,15 @@ public class StartPoint : MonoBehaviour
         //if the uuid matches the player uuid it teleport the player
         playerControllerScript.transform.position = transform.position;
         playerControllerScript.lastDirection = facingDirection;
+
+        GameObject confiner = GameObject.Find("Camera Confiner");
+
+        if (confiner != null)
+        {
+            Debug.Log("Existe el camera confiner");
+            virtualCameraObject.m_BoundingShape2D = confiner.GetComponent<PolygonCollider2D>();
+        }
+
     }
     void Update()
     {
